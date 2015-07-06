15c635872803285ad64db070294e5c9fbc9e1896
Yan Justino <yanlimaj@gmail.com>
2015-07-05 22:29:48 -0300

    fix identation SaleService

--- Sales/DDDStoreExemple.Sale.Core/Application/SaleService.cs
+++ Sales/DDDStoreExemple.Sale.Core/Application/SaleService.cs
@@ -7,8 +7,8 @@ namespace DDDStoreExemple.Sale.Core.Application
 {
     public class SaleService
     {
-		private readonly ICustomerRepository customerRepository;
-        
+        private readonly ICustomerRepository customerRepository;
+
         public SaleService(ICustomerRepository customerRepository)
         {
             this.customerRepository = customerRepository;
